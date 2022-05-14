c215ebd06779f6ca5d5f9347f6c0fe01edfa3e04
Abraham Bisrat <abrahammehari0@gmail.com>
2022-05-14 14:12:19 -0500

    Initial Commit

---
+++ src/main/java/edu/miu/cs/badgeandmembershipcontrol/BadgeAndMembershipControlApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.badgeandmembershipcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadgeAndMembershipControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadgeAndMembershipControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/cs/badgeandmembershipcontrol/BadgeAndMembershipControlApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.badgeandmembershipcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadgeAndMembershipControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
