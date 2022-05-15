e6d8b45eec6fad10268c121adbee72ee09f1bdc3
ChaimaaSill <chaimaasillahi@gmail.com>
2022-05-15 17:21:58 -0500

    Ticket#11 and Ticket#7

---
+++ src/main/java/edu/miu/cs/badgeandmembershipcontrol/controller/PlanController.java
@@ -0,0 +1,71 @@
+package edu.miu.cs.badgeandmembershipcontrol.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.miu.cs.badgeandmembershipcontrol.domain.Plan;
+import edu.miu.cs.badgeandmembershipcontrol.service.PlanService;
+
+@RestController
+@RequestMapping("/api/v1/plans")
+public class PlanController {
+    private PlanService planService;
+
+    public PlanController(PlanService planService){
+        this.planService = planService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<?> getMembers(){
+        List<Plan> planList = planService.getAllPlans();
+        return new ResponseEntity<>(planList, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{planId}")
+    public ResponseEntity<?> getPlan(@PathVariable String planId){
+        Plan plan = planService.getPlan(Long.parseLong(planId));
+
+        if(plan == null){
+            return new ResponseEntity<String>("No Plan Found!", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Plan>(plan, HttpStatus.OK);
+    }
+    
+    @GetMapping(path = "/location/{locationId}")
+    public ResponseEntity<?> getLocationPlans(@PathVariable String locationId){
+        List<Plan> planList = planService.getLocationPlans(Long.parseLong(locationId));
+        return new ResponseEntity<>(planList, HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> createPlan(@RequestBody Plan plan){
+        Plan plan1 = planService.createPlan(plan);
+        return new ResponseEntity<Plan>(plan1, HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/{planId}")
+    public ResponseEntity<?> updatePlan(@PathVariable String planId, @RequestBody Plan plan){
+        Plan plan1 = planService.updatePlan(Long.parseLong(planId), plan);
+        return new ResponseEntity<Plan>(plan1, HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{planId}")
+    public ResponseEntity<?> removePlan(@PathVariable String planId){
+        Boolean result = planService.removePlan(Long.parseLong(planId));
+        if(!result){
+            return new ResponseEntity<String>("No Plan Found!", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>("Successful", HttpStatus.OK);
+    }
+
+}
--- src/main/java/edu/miu/cs/badgeandmembershipcontrol/service/PlanService.java
+++ src/main/java/edu/miu/cs/badgeandmembershipcontrol/service/PlanService.java
@@ -1,4 +1,21 @@
 package edu.miu.cs.badgeandmembershipcontrol.service;
 
+import java.util.List;
+
+import edu.miu.cs.badgeandmembershipcontrol.domain.Plan;
+
 public interface PlanService {
+
+    List<Plan> getAllPlans();
+
+    Plan getPlan(Long planId);
+
+    List<Plan> getLocationPlans(Long locationId);
+
+    Plan createPlan(Plan plan);
+
+    Plan updatePlan(Long planId, Plan plan);
+
+    boolean removePlan(Long planId);
 }
+
