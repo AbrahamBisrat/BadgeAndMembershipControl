4eb0b5500f0ab5735210ce84408dadc216ae7c3f
EdiTeare <edomteare96@gmail.com>
2022-05-15 15:38:13 -0500

    woking on ticket 10

--- src/main/java/edu/miu/cs/badgeandmembershipcontrol/service/LocationService.java
+++ src/main/java/edu/miu/cs/badgeandmembershipcontrol/service/LocationService.java
@@ -1,4 +1,19 @@
 package edu.miu.cs.badgeandmembershipcontrol.service;
 
+import edu.miu.cs.badgeandmembershipcontrol.domain.Badge;
+import edu.miu.cs.badgeandmembershipcontrol.domain.Location;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public interface LocationService {
+    List<Location> getAllLocations();
+
+    Location getLocation(Long locationId);
+
+    Location createLocation(Location location);
+
+    Location updateLocation(Long locationId, Location location);
+
+    boolean removeLocation(Long locationId);
 }
