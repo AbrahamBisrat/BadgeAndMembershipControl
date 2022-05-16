72bbd9ed9bfcdcb135757e1569940475bb58c2a8
Abraham Bisrat <abrahammehari0@gmail.com>
2022-05-16 14:39:15 -0500

    Added more validation

--- src/main/java/edu/miu/cs/badgeandmembershipcontrol/service/Impl/MemberServiceImpl.java
+++ src/main/java/edu/miu/cs/badgeandmembershipcontrol/service/Impl/MemberServiceImpl.java
@@ -76,7 +76,7 @@ public class MemberServiceImpl implements MemberService {
             Long memberId, Long planId, Membership membership) {
         Plan plan = planService.getPlan(planId);
         Member member = memberRepository.findById(memberId).orElse(null);
-        if(member == null || plan == null){
+        if(member == null || plan == null || membership == null){
             System.out.println("Bad request, The id belong to nothing");
             return null;
         }
