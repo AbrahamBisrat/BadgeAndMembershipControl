1de1b45dbdcbaad19833fe6239925b751da4c3c2
Abraham Bisrat <abrahammehari0@gmail.com>
2022-05-15 22:10:37 -0500

    refactored plan controller

--- src/main/java/edu/miu/cs/badgeandmembershipcontrol/controller/PlanController.java
+++ src/main/java/edu/miu/cs/badgeandmembershipcontrol/controller/PlanController.java
@@ -2,6 +2,7 @@ package edu.miu.cs.badgeandmembershipcontrol.controller;
 
 import java.util.List;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,18 +18,15 @@ import edu.miu.cs.badgeandmembershipcontrol.domain.Plan;
 import edu.miu.cs.badgeandmembershipcontrol.service.PlanService;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/plans")
 public class PlanController {
-    private PlanService planService;
 
-    public PlanController(PlanService planService){
-        this.planService = planService;
-    }
+    private final PlanService planService;
 
     @GetMapping()
     public ResponseEntity<?> getMembers(){
-        List<Plan> planList = planService.getAllPlans();
-        return new ResponseEntity<>(planList, HttpStatus.OK);
+        return new ResponseEntity<>(planService.getAllPlans(), HttpStatus.OK);
     }
 
     @GetMapping(path = "/{planId}")
@@ -36,9 +34,9 @@ public class PlanController {
         Plan plan = planService.getPlan(Long.parseLong(planId));
 
         if(plan == null){
-            return new ResponseEntity<String>("No Plan Found!", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("No Plan Found!", HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<Plan>(plan, HttpStatus.OK);
+        return new ResponseEntity<>(plan, HttpStatus.OK);
     }
     
     @GetMapping(path = "/location/{locationId}")
@@ -49,20 +47,19 @@ public class PlanController {
 
     @PostMapping()
     public ResponseEntity<?> createPlan(@RequestBody Plan plan){
-        Plan plan1 = planService.createPlan(plan);
-        return new ResponseEntity<Plan>(plan1, HttpStatus.OK);
+        Plan newPlan = planService.createPlan(plan);
+        return new ResponseEntity<>(newPlan, HttpStatus.OK);
     }
 
     @PutMapping(path = "/{planId}")
     public ResponseEntity<?> updatePlan(@PathVariable String planId, @RequestBody Plan plan){
-        Plan plan1 = planService.updatePlan(Long.parseLong(planId), plan);
-        return new ResponseEntity<Plan>(plan1, HttpStatus.OK);
+        Plan updatedPlan = planService.updatePlan(Long.parseLong(planId), plan);
+        return new ResponseEntity<>(updatedPlan, HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/{planId}")
     public ResponseEntity<?> removePlan(@PathVariable String planId){
-        Boolean result = planService.removePlan(Long.parseLong(planId));
-        if(!result){
+        if(!planService.removePlan(Long.parseLong(planId))){
             return new ResponseEntity<String>("No Plan Found!", HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>("Successful", HttpStatus.OK);
