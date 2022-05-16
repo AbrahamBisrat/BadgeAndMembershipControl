c4f069b805810c93a80e81890a9fd7765c011a7a
ChaimaaSill <chaimaasillahi@gmail.com>
2022-05-16 14:58:51 -0500

    Ticket#26

--- src/main/java/edu/miu/cs/badgeandmembershipcontrol/repository/TransactionRepository.java
+++ src/main/java/edu/miu/cs/badgeandmembershipcontrol/repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package edu.miu.cs.badgeandmembershipcontrol.repository;
 
 import java.util.List;
 import java.util.Optional;
+
+import edu.miu.cs.badgeandmembershipcontrol.domain.Location;
 import edu.miu.cs.badgeandmembershipcontrol.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
 	Optional<List<Transaction>> findTransactionsByBadge_Id(Long badgelocationId);
+	Optional<Location> findTransactionLocationBy_Id(Long transactionId);
 }
--- src/main/java/edu/miu/cs/badgeandmembershipcontrol/service/Impl/TransactionServiceImpl.java
+++ src/main/java/edu/miu/cs/badgeandmembershipcontrol/service/Impl/TransactionServiceImpl.java
@@ -1,6 +1,7 @@
 package edu.miu.cs.badgeandmembershipcontrol.service.Impl;
 
 import com.sun.istack.NotNull;
+import edu.miu.cs.badgeandmembershipcontrol.domain.Location;
 import edu.miu.cs.badgeandmembershipcontrol.domain.Transaction;
 import edu.miu.cs.badgeandmembershipcontrol.repository.TransactionRepository;
 import edu.miu.cs.badgeandmembershipcontrol.service.TransactionService;
@@ -45,5 +46,11 @@ public class TransactionServiceImpl implements TransactionService {
 		}
 		return false;
 	}
+
+	@Override public Location getTransactionLocation(Long transactionId) {
+		Optional<Location> transactionLocationOptional = transactionRepository.findTransactionLocationBy_Id(transactionId);
+		return transactionLocationOptional.orElse(null);
+	}
+
 
 }
