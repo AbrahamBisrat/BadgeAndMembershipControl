84c2834fb695c9bc29dbe6aeb7d0558ead15ae4f
Abraham Bisrat <abrahammehari0@gmail.com>
2022-05-17 16:50:45 -0500

    removed redundencies in @ResponseBody<>

--- src/main/java/edu/miu/cs/badgeandmembershipcontrol/controller/MemberController.java
+++ src/main/java/edu/miu/cs/badgeandmembershipcontrol/controller/MemberController.java
@@ -22,7 +22,6 @@ public class MemberController {
 
     @GetMapping()
     public ResponseEntity<?> getMembers(){
-
         List<Member> memberList = memberService.getAllMembers();
         return new ResponseEntity<>(memberList, HttpStatus.OK);
     }
@@ -31,40 +30,37 @@ public class MemberController {
     public ResponseEntity<?> getMember(@PathVariable Long memberId){
         Member member = memberService.getMember(memberId);
         if(member == null){
-            return new ResponseEntity<String>("No Member Found!", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("No Member Found!", HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<Member>(member, HttpStatus.OK);
+        return new ResponseEntity<>(member, HttpStatus.OK);
     }
 
 
     @GetMapping(path = "/{memberId}/memberships")
     public ResponseEntity<?> getMembershipsByMember(@PathVariable String memberId){
         List<Membership> memberships = memberService.getMembershipsByMemberId(Long.parseLong(memberId));
-
         if(memberships.isEmpty()){
-            return new ResponseEntity<String>("No Membership Found!", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("No Membership Found!", HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<List<Membership>>(memberships, HttpStatus.OK);
+        return new ResponseEntity<>(memberships, HttpStatus.OK);
     }
 
     @GetMapping(path = "/{memberId}/badges")
     public ResponseEntity<?> getBadgeByMember(@PathVariable String memberId){
         List<Badge> badgeList = memberService.getBadgesByMember(Long.parseLong(memberId));
-
         if(badgeList.isEmpty()){
-            return new ResponseEntity<String>("No Badge Found!", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("No Badge Found!", HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<List<Badge>>(badgeList, HttpStatus.OK);
+        return new ResponseEntity<>(badgeList, HttpStatus.OK);
     }
 
     @GetMapping(path = "/{memberId}/activeBadge")
     public ResponseEntity<?> getActiveBadge(@PathVariable String memberId){
         Badge badge = memberService.getActiveBadgeByMember(Long.parseLong(memberId));
-
         if(badge == null){
-            return new ResponseEntity<String>("No Active Badge Found!", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("No Active Badge Found!", HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<Badge>(badge, HttpStatus.OK);
+        return new ResponseEntity<>(badge, HttpStatus.OK);
     }
 
     @PostMapping()
@@ -78,33 +74,33 @@ public class MemberController {
     @PutMapping(path = "/{memberId}")
     public ResponseEntity<?> updateMember(@PathVariable Long memberId, @RequestBody Member member){
         if(memberService.getMember(memberId) == null) {
-            return new ResponseEntity<String>("No member by the Id " + memberId + " found", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("No member by the Id " + memberId + " found", HttpStatus.NOT_FOUND);
         }
         Member updatedMember = memberService.updateMember(memberId , member);
-        return new ResponseEntity<Member>(updatedMember, HttpStatus.OK);
+        return new ResponseEntity<>(updatedMember, HttpStatus.OK);
     }
 
     @PostMapping(path = "/{memberId}/renewBadge")
     public ResponseEntity<?> renewMemberBadge(@PathVariable Long memberId){
         if(memberService.getMember(memberId) == null) {
-            return new ResponseEntity<String>("No member by the Id "
+            return new ResponseEntity<>("No member by the Id "
                     + memberId + " found", HttpStatus.NOT_FOUND);
         }
         Member member = memberService.createNewBadge(memberId);
-        return new ResponseEntity<Member>(member, HttpStatus.OK);
+        return new ResponseEntity<>(member, HttpStatus.OK);
     }
 
     @PostMapping(path = "/invokeMembership")
     public ResponseEntity<?> invokeMembership(@PathParam("memberId") Long memberId,
                                               @PathParam("membershipId") Long membershipId){
         Member member = memberService.deActivateMembership(memberId , membershipId);
-        return new ResponseEntity<Member>(member, HttpStatus.OK);
+        return new ResponseEntity<>(member, HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/{memberId}")
     public ResponseEntity<?> deleteMember(@PathVariable Long memberId){
         if(!memberService.removeMember(memberId)){
-            return new ResponseEntity<String>("No Member Found!", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("No Member Found!", HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>("Successful", HttpStatus.OK);
     }
